9c5e8bcb5382599ee946cfb60971aeac3378a2e1
sarthakpranesh <sarthak.pranesh2018@vitstudent.ac.in>
2020-12-05 19:21:46 +0530

    Standalone app build and ios configs added
    Signed-off-by: sarthakpranesh <sarthak.pranesh2018@vitstudent.ac.in>

--- HummingNote/components/GoogleSignIn.tsx
+++ HummingNote/components/GoogleSignIn.tsx
@@ -5,8 +5,9 @@ export default async function GoogleSignIn() {
     return new Promise(async (resolve, reject) => {
         try {
             const result = await Google.logInAsync({
-              androidClientId: `1034297543601-kp3hggjodllcecmj3iljs4pc9ptkvvvo.apps.googleusercontent.com`,
-              androidStandaloneAppClientId: `1034297543601-kp3hggjodllcecmj3iljs4pc9ptkvvvo.apps.googleusercontent.com`,
+              androidClientId: '1034297543601-3400h905sl31hliqrq3kqkq4ld7jika8.apps.googleusercontent.com',
+              iosClientId: '1034297543601-dta6hejks2d3j11cmmc7el05p17creah.apps.googleusercontent.com',
+              androidStandaloneAppClientId: "",
               scopes: ['profile', 'email'],
             });
         
